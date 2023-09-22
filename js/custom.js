d4ff857171be3d5570676eeb818130c78332e2ee
Gaurav <hey9aurav@gmail.com>
2023-09-22 14:02:03 +0530

    Popup and news backend implemented.

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,43 @@
-$('.modal').modal('toggle')
+var param = {
+    "param": {
+        "type": 'web_popup'
+    }
+}
+
+async function getData() {
+    const popup = await fetch('http://localhost:3000/api/GetGeneralSettings', {
+        method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        body: JSON.stringify(param),
+    })
+    const popupData = await popup.json();
+    if (popupData.data[0].status) {
+        document.getElementById('message').value = popupData.data[0].message
+        $('.modal').modal('toggle')
+    }
+    
+    param = {
+        "param": {
+            "type": 'web_news'
+        }
+    }
+    const news = await fetch('http://localhost:3000/api/GetGeneralSettings', {
+        method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        body: JSON.stringify(param),
+    })
+    const newsData = await news.json();
+    if (newsData.data[0].status) {
+        document.getElementById('news-text').innerText = newsData.data[0].message   
+    } else {
+        document.getElementById('news_section').style.display = 'none'
+    }
+}
+
+getData();
